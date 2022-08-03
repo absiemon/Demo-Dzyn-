b21f491b4ac49939ab316153d818a91a3e9356b5
alkakumari12 <alka58406@gmail.com>
2022-08-03 19:39:42 +0530

    Initialize project using Create React App

---
+++ BackEnd/index.js
@@ -0,0 +1,16 @@
+const connectToMongo = require('./db');
+const express = require('express')
+var cors = require('cors');
+connectToMongo();
+
+const app = express();
+app.use(cors());
+const port = 5000;
+
+app.use(express.json());  // middleware to read the json data form the req
+
+app.use('/api/auth', require('./routes/auth'));
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  })
